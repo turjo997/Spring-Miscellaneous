58eaf487505a415782f9338503ecf1229b81ea10
turjo997 <turja997@gmail.com>
2024-07-15 17:11:10 +0600

    redis-cache project added

---
+++ spring-redis-cache/src/main/java/com/spring/spring_redis_cache/SpringRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_redis_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisCacheApplication.class, args);
+	}
+
+}
---
+++ spring-redis-cache/src/test/java/com/spring/spring_redis_cache/SpringRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring_redis_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
