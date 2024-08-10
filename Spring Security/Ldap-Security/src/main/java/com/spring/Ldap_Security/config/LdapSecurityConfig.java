7aef81837ccebd8273936b8fdce78b1f24c79a5e
turjo997 <turja997@gmail.com>
2024-08-10 15:36:45 +0600

    ldap-security updated

--- Spring Security/Ldap-Security/src/main/java/com/spring/Ldap_Security/config/LdapSecurityConfig.java
+++ Spring Security/Ldap-Security/src/main/java/com/spring/Ldap_Security/config/LdapSecurityConfig.java
@@ -1,25 +1,40 @@
 package com.spring.Ldap_Security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class LdapSecurityConfig {
 
-    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests()
                 .anyRequest().fullyAuthenticated()
                 .and()
-                .formLogin();
+                .formLogin(Customizer.withDefaults());
 
         //httpSecurity.authenticationProvider();
 
         return httpSecurity.build();
     }
 
-
-
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
+    }
 }
