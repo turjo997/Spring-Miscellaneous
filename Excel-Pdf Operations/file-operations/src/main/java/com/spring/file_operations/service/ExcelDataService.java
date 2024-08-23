459b3a8e952cb45e355495af838b38cfbc9726dc
turjo997 <turja997@gmail.com>
2024-08-23 19:51:56 +0600

    saved data for every .sxlx file

--- Excel-Pdf Operations/file-operations/src/main/java/com/spring/file_operations/service/ExcelDataService.java
+++ Excel-Pdf Operations/file-operations/src/main/java/com/spring/file_operations/service/ExcelDataService.java
@@ -12,6 +12,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ExcelDataService {
@@ -21,60 +23,76 @@ public class ExcelDataService {
     @Value("${app.upload.file:${user.home}}")
     public String EXCEL_FILE_PATH;
 
+
+    @Value("${app.upload.dir:${user.home}}")
+    public String uploadDir;
+
     public ExcelDataService(InvoiceRepository invoiceRepository) {
         this.invoiceRepository = invoiceRepository;
     }
 
 
     public List<Invoice> getExcelDataAsList(){
-        List<String> list = new ArrayList<>();
+        List<Invoice> allInvList = new ArrayList<>();
         Workbook workbook = null;
 
-         // Create a DataFormatter to format and get each cell's value as String
+
+        File folder = new File(uploadDir);
+        File[] files = folder.listFiles(((dir, name) -> name.endsWith(".xlsx")));
+
         DataFormatter dataFormatter = new DataFormatter();
 
-        System.out.println(EXCEL_FILE_PATH);
+        if (files != null && files.length  > 0){
 
-        // Create the Workbook
-        try {
-            workbook = WorkbookFactory.create(new File(EXCEL_FILE_PATH));
-        }catch (EncryptedDocumentException | IOException e){
-            e.printStackTrace();
-        }
+            for (File file : files){
+                System.out.println("Processing file: " + file.getAbsolutePath());
 
-        // Retrieving the number of sheets in the Workbook
-        System.out.println("-------Workbook has '" + workbook.getNumberOfSheets() + "' Sheets-----");
-
-
-        // Getting the Sheet at index zero
-        Sheet sheet = workbook.getSheetAt(0);
-
-        // Getting number of columns in the Sheet
-        int noOfColumns = sheet.getRow(0).getLastCellNum();
-        System.out.println("-------Sheet has '"+noOfColumns+"' columns------");
-
-        // Using for-each loop to iterate over the rows and columns
-
-        for (Row row : sheet){
-            for (Cell cell : row){
-                String cellValue = dataFormatter.formatCellValue(cell).trim();
-                if (!cellValue.isEmpty()) {
-                    list.add(cellValue);
+                try {
+                    workbook = WorkbookFactory.create(file);
+                }catch (EncryptedDocumentException | IOException e){
+                    e.printStackTrace();
                 }
+
+                // Getting the Sheet at index zero
+                Sheet sheet = workbook.getSheetAt(0);
+
+                // Getting number of columns in the Sheet
+                int noOfColumns = sheet.getRow(0).getLastCellNum();
+                System.out.println("-------Sheet has '"+noOfColumns+"' columns------");
+
+                List<String> list = new ArrayList<>();
+
+                // Using for-each loop to iterate over the rows and columns
+
+                for (Row row : sheet){
+                    for (Cell cell : row){
+                        String cellValue = dataFormatter.formatCellValue(cell).trim();
+                        if (!cellValue.isEmpty()) {
+                            list.add(cellValue);
+                        }
+                    }
+                }
+
+                // filling excel data and creating list as List<Invoice>
+                List<Invoice> invList = createList(list , noOfColumns);
+
+
+                // Closing the workbook
+                try {
+                    workbook.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+                allInvList.addAll(invList);
             }
+
+            return allInvList;
+
+        }else {
+            System.out.println("No .xlsx files found in the directory.");
+            return Collections.emptyList();
         }
-
-        // filling excel data and creating list as List<Invoice>
-        List<Invoice> invList = createList(list , noOfColumns);
-
-        // Closing the workbook
-        try {
-            workbook.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
-        return invList;
     }
 
     private List<Invoice> createList(List<String> excelData, int noOfColumns) {
