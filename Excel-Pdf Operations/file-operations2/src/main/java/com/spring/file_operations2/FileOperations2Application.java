12f39f8d50781ae7d89647f1346a1250b28f62d4
turjo997 <turja997@gmail.com>
2024-08-28 18:39:40 +0600

    file operations2 added

---
+++ Excel-Pdf Operations/file-operations2/src/main/java/com/spring/file_operations2/FileOperations2Application.java
@@ -0,0 +1,13 @@
+package com.spring.file_operations2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileOperations2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileOperations2Application.class, args);
+	}
+
+}
---
+++ Excel-Pdf Operations/file-operations2/src/test/java/com/spring/file_operations2/FileOperations2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.file_operations2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileOperations2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
