54efe31b51aa5a86b1d6af94d141c031031cd9ea
turjo997 <turja997@gmail.com>
2024-09-15 14:18:11 +0600

    updated

--- spring-kubernetes/src/main/java/spring/kubernetes/spring_kubernetes/repo/BookRepo.java
+++ spring-kubernetes/src/main/java/spring/kubernetes/spring_kubernetes/repo/BookRepo.java
@@ -1,8 +1,9 @@
 package spring.kubernetes.spring_kubernetes.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import spring.kubernetes.spring_kubernetes.entity.Book;
-
+@Repository
 public interface BookRepo extends JpaRepository<Book, Integer> {
 
 }
